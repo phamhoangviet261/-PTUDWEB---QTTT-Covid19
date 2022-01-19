a647a75903554533db04f9d12513b7514ddfc554
quocthinhred <tranquocthinh.t2.15cla@gmail.com>
2022-01-19 15:21:49 +0700

    search on product

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -18,6 +18,30 @@ const wardModel = require('../models/ward.model')
 
 const router = require('express').Router();
 
+function nonAccentVietnamese(str) {
+    str = str.toLowerCase();
+//     We can also use this instead of from line 11 to line 17
+//     str = str.replace(/\u00E0|\u00E1|\u1EA1|\u1EA3|\u00E3|\u00E2|\u1EA7|\u1EA5|\u1EAD|\u1EA9|\u1EAB|\u0103|\u1EB1|\u1EAF|\u1EB7|\u1EB3|\u1EB5/g, "a");
+//     str = str.replace(/\u00E8|\u00E9|\u1EB9|\u1EBB|\u1EBD|\u00EA|\u1EC1|\u1EBF|\u1EC7|\u1EC3|\u1EC5/g, "e");
+//     str = str.replace(/\u00EC|\u00ED|\u1ECB|\u1EC9|\u0129/g, "i");
+//     str = str.replace(/\u00F2|\u00F3|\u1ECD|\u1ECF|\u00F5|\u00F4|\u1ED3|\u1ED1|\u1ED9|\u1ED5|\u1ED7|\u01A1|\u1EDD|\u1EDB|\u1EE3|\u1EDF|\u1EE1/g, "o");
+//     str = str.replace(/\u00F9|\u00FA|\u1EE5|\u1EE7|\u0169|\u01B0|\u1EEB|\u1EE9|\u1EF1|\u1EED|\u1EEF/g, "u");
+//     str = str.replace(/\u1EF3|\u00FD|\u1EF5|\u1EF7|\u1EF9/g, "y");
+//     str = str.replace(/\u0111/g, "d");
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng 
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+    return str;
+}
+
+
 router.use(function (req, res, next) {
     if(req.body.username && req.body.password){
         if(req.body.username == "admin" && req.body.password == "admin"){
@@ -251,6 +275,12 @@ router.get('/manage-product', async (req, res, next) => {
         let temp = product.MaSP.substring(product.MaSP.length - 3) - 0;
         product["key"] = temp;
     })
+    let max = 0;
+    lp.forEach(item => {
+        if (item.key >= max){
+            max = item.key;
+        }
+    })
     res.render('admin/index', {
         cssP: () => 'css',
         scriptsP: () => 'script',
@@ -258,7 +288,7 @@ router.get('/manage-product', async (req, res, next) => {
         footerP: () => 'footer',
         isManageProduct: true,
         products: lp,
-        newProductID: lp[lp.length-1].MaSP.substring(0, lp[lp.length-1].MaSP.length - 3) + '0' + (lp.length + 1).toString()
+        newProductID: lp[lp.length-1].MaSP.substring(0, lp[lp.length-1].MaSP.length - 3) + '0' + (max + 1).toString()
     });
 })
 
@@ -278,6 +308,42 @@ router.post('/manage-product/delete', async (req, res, next) => {
     let product = req.body;
     let temp = await productModel.delete(product.MaSP);
     res.json(product);
+})
+
+router.post('/manage-product/search', async (req, res, next) => {
+    let result = [];
+    let strSearch = nonAccentVietnamese(req.body.search);
+    console.log(strSearch);
+    console.log(req.body.type);
+    let lp = await productModel.all();
+    lp.forEach(item => {
+        item['newTenSP'] = nonAccentVietnamese(item.TenSP);
+    })
+    if (req.body.type == 'id'){
+        result = lp.filter(item => nonAccentVietnamese(item.MaSP).includes(strSearch));
+    }
+    else {
+        result = lp.filter(item => item.newTenSP.includes(strSearch));
+    }
+    lp.map(product => {
+        let temp = product.MaSP.substring(product.MaSP.length - 3) - 0;
+        product["key"] = temp;
+    })
+    let max = 0;
+    lp.forEach(item => {
+        if (item.key >= max){
+            max = item.key;
+        }
+    })
+    res.render('admin/index', {
+        cssP: () => 'css',
+        scriptsP: () => 'script',
+        navP: () => 'nav',
+        footerP: () => 'footer',
+        isManageProduct: true,
+        products: result,
+        newProductID: lp[lp.length-1].MaSP.substring(0, lp[lp.length-1].MaSP.length - 3) + '0' + (max + 1).toString()
+    });
 })
 
 router.get('/manage-neccessary', async (req, res, next) => {
